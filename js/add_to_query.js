2f9ea56ba8004ccc5386eeedc07ea520bef077e0
vachanta93 <achanta.vidya@gmail.com>
2014-12-04 14:29:07 -0600

    Changed UI elements to Bootstrap
    Changed elements in index.html, style.css, and add_to_query.js. Added
    all necessary Bootstrap files in new lib folder. Commented out old HTML
    in index.html. May remove later.

--- js/add_to_query.js
+++ js/add_to_query.js
@@ -1,15 +1,13 @@
-function addToList(TreeText) 
-{
-var table = document.getElementById("listTable");
-var row = table.insertRow(0);
-var textCell = row.insertCell(0);
-var btnCell = row.insertCell(1);
-textCell.innerHTML = "<p>TreeText</p>";
-btnCell.innerHTML = "<button onclick='delRow(this)'>X</button>";//Delete Button
-
+function addToList(TreeText) {
+	var table = document.getElementById("listTable");
+	var row = table.insertRow(0);
+	var textCell = row.insertCell(0);
+	var btnCell = row.insertCell(1);
+	textCell.innerHTML = "<p>TreeText</p>";
+	btnCell.innerHTML = "<button type='button' class='btn btn-danger' data-toggle='button' aria-pressed='false' autocomplete='off' onclick='delRow(this)'>Delete</button>";//Delete Button
 }
 
 function delRow(o) {
-	var p=o.parentNode.parentNode;
-         p.parentNode.removeChild(p);
+	var p = o.parentNode.parentNode;
+	p.parentNode.removeChild(p);
 }
