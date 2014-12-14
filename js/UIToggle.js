e8f74e0c271c65ce05805330081d560cd6b94d3b
Tony Giangiulio <agiangiulio@mail.bradley.edu>
2014-12-13 18:55:09 -0600

    Toggle out the UI.

---
+++ js/UIToggle.js
@@ -0,0 +1,8 @@
+function toggleAll() {
+	$( ".btn-group" ).fadeToggle( "slow", "linear" );
+	$( "#QueryListing" ).fadeToggle( "slow", "linear" );
+	$( ".btn btn-danger" ).fadeToggle( "slow", "linear" );
+	$( ".dropdown" ).fadeToggle( "slow", "linear" );
+	$( ".form-group" ).fadeToggle( "slow", "linear" );
+	$( "#submit" ).fadeToggle( "slow", "linear" );
+}
