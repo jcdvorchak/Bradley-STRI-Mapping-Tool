6b00d721495491eb3d6b261e11a7f1f784e0f70d
vachanta93 <achanta.vidya@gmail.com>
2014-12-15 18:48:46 -0600

    Can add species to each country tab individually

--- js/add_to_query.js
+++ js/add_to_query.js
@@ -1,13 +1,20 @@
 function addToList(TreeText) {
-	var activeTab = document.getElementsByClassName('tab-pane active'); //doesn't do anything yet
+	var table;
 
-	var table = document.getElementById("listTable");
+	var activeTab = document.getElementsByClassName('tab-pane active')[0];
+	if(activeTab.id == "panama") {
+		table = document.getElementById("listTablePanama");
+	}
+	else if(activeTab.id == "colombia") {
+		table = document.getElementById("listTableColombia");
+	}
+
 	var row = table.insertRow(0);
 	var textCell = row.insertCell(0);
 	var btnCell = row.insertCell(1);
 	var treeText = document.getElementById('search_box').value;
 	textCell.innerHTML = treeText;
-	btnCell.innerHTML = "<button type='button' class='btn btn-primary' data-toggle='button' aria-pressed='false' autocomplete='off' onclick='delRow(this)'>Delete</button>";//Delete Button
+	btnCell.innerHTML = "<button type='button' class='btn btn-success' data-toggle='button' aria-pressed='false' autocomplete='off' onclick='delRow(this)'>Delete</button>";//Delete Button
 }
 
 function delRow(o) {
