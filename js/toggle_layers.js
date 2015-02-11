32ba2b777e6a644f08bea57456dbd70b67453ab3
vachanta93 <achanta.vidya@gmail.com>
2015-02-10 18:54:39 -0600

    Rivers, Admin, & Protected layers come on and off

--- js/toggle_layers.js
+++ js/toggle_layers.js
@@ -1,49 +1,64 @@
-function riverLayer() {
-	var layer = new google.maps.FusionTablesLayer({
-		query: {
-			select: 'geometry',
-			from: '1vCvnicNK4hdsgq5mP4CKuoeYQOouCOd0_VwH93Mx'
-		},
-		styles: [{
-			polylineOptions: {
-				strokeColor: '#0000ff',
-				strokeWeight: '1'
-			}
-		}]
-	});
-	layer.setMap(map);
-}
+var riverLayer = new google.maps.FusionTablesLayer({
+	query: {
+		select: 'geometry',
+		from: '1vCvnicNK4hdsgq5mP4CKuoeYQOouCOd0_VwH93Mx'
+	},
+	styles: [{
+		polylineOptions: {
+			strokeColor: '#0000ff',
+			strokeWeight: '1'
+		}
+	}]
+});
 
-function adminLayer() {
-	var layer = new google.maps.FusionTablesLayer({
-		query: {
-			select: 'geometry',
-			from: '15IjvsOUlWW0v2uRhCUbrEv2xNw2r0yBJ0t-1wO-Q'
-		},
-		styles: [{
-			polygonOptions: {
-				fillColor: '#B2DFEE',
-				fillOpacity: 0.2
-			}
-		}]
-	});
-	layer.setMap(map);
-}
+var adminLayer = new google.maps.FusionTablesLayer({
+	query: {
+		select: 'geometry',
+		from: '15IjvsOUlWW0v2uRhCUbrEv2xNw2r0yBJ0t-1wO-Q'
+	},
+	styles: [{
+		polygonOptions: {
+			fillColor: '#B2DFEE',
+			fillOpacity: 0.2
+		}
+	}]
+});
 
-function protectedLayer() {
-	var layer = new google.maps.FusionTablesLayer({
-		query: {
-			select: 'geometry',
-			from: '1c7CVWeo2XSA-SjzIIHtqtCtiPFU_fx64G3UtnBy2'
-		},
-		styles: [{
-			polygonOptions: {
-				fillColor: '#4a777a',
-				fillOpacity: 0.2
-			}
-		}]
-	});
-	layer.setMap(map);
+var protectedLayer = new google.maps.FusionTablesLayer({
+	query: {
+		select: 'geometry',
+		from: '1c7CVWeo2XSA-SjzIIHtqtCtiPFU_fx64G3UtnBy2'
+	},
+	styles: [{
+		polygonOptions: {
+			fillColor: '#4a777a',
+			fillOpacity: 0.2
+		}
+	}]
+});
+
+function changeLayer() {
+	var rLayer = document.getElementById("layer1").checked;
+	var aLayer = document.getElementById("layer2").checked;
+	var pLayer = document.getElementById("layer3").checked;
+	
+	if(rLayer == true) {
+		riverLayer.setMap(map);
+	} else if(rLayer == false) {
+		riverLayer.setMap(null);
+	} 
+
+	if(aLayer == true) {
+		adminLayer.setMap(map);
+	} else if(aLayer == false) {
+		adminLayer.setMap(null);
+	} 
+
+	if(pLayer == true) {
+		protectedLayer.setMap(map);
+	} else if(pLayer == false) {
+		protectedLayer.setMap(null);
+	}
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
