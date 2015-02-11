9d66d7fda77b44906c1f923422afce42ca8bf921
vachanta93 <achanta.vidya@gmail.com>
2015-02-10 19:03:22 -0600

    Rivers, Admin, Protected layers work at same time

--- js/toggle_layers.js
+++ js/toggle_layers.js
@@ -3,12 +3,7 @@ var riverLayer = new google.maps.FusionTablesLayer({
 		select: 'geometry',
 		from: '1vCvnicNK4hdsgq5mP4CKuoeYQOouCOd0_VwH93Mx'
 	},
-	styles: [{
-		polylineOptions: {
-			strokeColor: '#0000ff',
-			strokeWeight: '1'
-		}
-	}]
+	styleId:2
 });
 
 var adminLayer = new google.maps.FusionTablesLayer({
@@ -16,12 +11,7 @@ var adminLayer = new google.maps.FusionTablesLayer({
 		select: 'geometry',
 		from: '15IjvsOUlWW0v2uRhCUbrEv2xNw2r0yBJ0t-1wO-Q'
 	},
-	styles: [{
-		polygonOptions: {
-			fillColor: '#B2DFEE',
-			fillOpacity: 0.2
-		}
-	}]
+	styleId: 2
 });
 
 var protectedLayer = new google.maps.FusionTablesLayer({
@@ -29,12 +19,7 @@ var protectedLayer = new google.maps.FusionTablesLayer({
 		select: 'geometry',
 		from: '1c7CVWeo2XSA-SjzIIHtqtCtiPFU_fx64G3UtnBy2'
 	},
-	styles: [{
-		polygonOptions: {
-			fillColor: '#4a777a',
-			fillOpacity: 0.2
-		}
-	}]
+	styleId: 2
 });
 
 function changeLayer() {
