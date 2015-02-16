3c292291ca8215f81a4e12b9d3d4b12f686823c0
Tony Giangiulio <agiangiulio@mail.bradley.edu>
2015-02-15 18:47:18 -0600

    small work

--- js/populate_genus_dropdown.js
+++ js/populate_genus_dropdown.js
@@ -1,4 +1,8 @@
-var ul = document.getElementById("list");
+var ul = document.getElementById("genus");
 var li = document.createElement("li");
+
+
+
+//loop or somethign through the table
 li.appendChild(document.createTextNode("Four"));
 ul.appendChild(li);
