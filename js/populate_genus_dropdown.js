664cd17c67650c2754f96429347b3079b5f18391
GigaJake <GigaJake@gmail.com>
2015-02-16 23:14:55 -0600

    Populated Genus dropdown
    Genus drop down list is populated properly, clicking on an item will
    display an alert message

--- js/populate_genus_dropdown.js
+++ js/populate_genus_dropdown.js
@@ -1,58 +1,40 @@
-// var ul = document.getElementById("genus");
-// var li = document.createElement("li");
+function genus_populate ()
+{
+    var ul = document.getElementById("genuslist");
+    if(ul){
+        while(ul.firstChild){
+            ul.removeChild(ul.firstChild);
+        }
+    }
 
-
-
-// //loop or somethign through the table
-// li.appendChild(document.createTextNode("Four"));
-// ul.appendChild(li);
-
-
-
-function SearchAJA() {
-
-}
-
-
-SearchAJA.prototype.searchAJAHandler = function () {
-//WILL PROBABLY USE SOMETHING LIKE THIS VVVVVVVVV
-
-     $(function () { 
-            var query = "SELECT 'Genus' FROM " +
+    var query = "SELECT 'Genus' FROM " +
                 '1EtC8wMoso-d59wgiTgXaGFTovW2-wcgdb25jNV8p' +
                 //" WHERE 'Genus' LIKE " + "'" + searchValue + "%'" +
                 " GROUP BY 'Genus'";
-            var encodedQuery = encodeURIComponent(query);
+    var encodedQuery = encodeURIComponent(query);
 
             // Construct the URL
-            var url = ['https://www.googleapis.com/fusiontables/v1/query'];
-            url.push('?sql=' + encodedQuery);
-            url.push('&key=AIzaSyCnxStZYPcxJNBjAa7V96g__7lpv80jIMY');
-            url.push('&callback=?');
+    var url = ['https://www.googleapis.com/fusiontables/v1/query'];
+    url.push('?sql=' + encodedQuery);
+    url.push('&key=AIzaSyCnxStZYPcxJNBjAa7V96g__7lpv80jIMY');
+    url.push('&callback=?');
 
-            $.ajax({
-                url: url.join(''),
-                dataType: 'jsonp',
-                success: function (data) {
-                    var rows = data['rows'];
-                    var dataArray1 = [];
-                    for (i in rows) {
-                        var ele1 = rows[i][0]
-                        dataArray1.push(ele1);
-                    }
-
-                      */
-                    $("#genuslist").menu({   //CHANGED TO GENUS,
-                        source: dataArray1
-                    });
-                    //}
-                }
-            });
-        })
+    $.ajax({
+        url: url.join(''),
+        dataType: 'jsonp',
+        success: function (data) 
+        {
+            var rows = data['rows'];
+            
+            for (i in rows) 
+            {
+                var ele1 = rows[i][0]
+                var node = document.createElement("li");
+                var textnode = document.createTextNode(ele1);
+                node.appendChild(textnode);
+                node.setAttribute("id",ele1);
+                ul.appendChild(node);
+            }
+        }
     });
-};
-
-(function() {
-  var search = new SearchAJA();
-  search.searchAJAHandler();
-})();
+}
